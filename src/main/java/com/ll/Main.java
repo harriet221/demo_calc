3a17fcb55b300cffb3ea2335e76b7e2bd94f30b3
harriet221 <potter19@dongguk.edu>
2023-10-30 10:04:11 +0900

    싹 갈아엎고 TDD 적용하며 다시

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -10,38 +10,6 @@ public class Main {
 
 
 class Calc {
-    static int run(String form) {
-        String[] forms = form.split(" ");
-        Stack<Double> stack = new Stack<Double>();
-        double answer = 0;
-        String[] operation = {"+", "-", "*", "/"};
 
-        for (int i = 0; i < forms.length; i++) {
-            if (forms[i].equals("+")) {
-                double a = stack.pop();
-                double b = Double.parseDouble(forms[++i]);
-                stack.push(a + b);
-            } else if (forms[i].equals("-")) {
-                double a = stack.pop();
-                double b = Double.parseDouble(forms[++i]);
-                stack.push(a - b);
-            } else if (forms[i].equals("*")) {
-                double a = stack.pop();
-                double b = Double.parseDouble(forms[++i]);
-                stack.push(a * b);
-            } else if (forms[i].equals("/")) {
-                double a = stack.pop();
-                double b = Double.parseDouble(forms[++i]);
-                if(b == 0) {
-                    System.out.println("0으로 나누면 안됨");
-                    return -1;
-                }
-                stack.push(a / b);
-            } else {
-                stack.push(Double.parseDouble(forms[i]));
-            }
-        }
-        answer = stack.pop();
-        return (int)answer;
-    }
+
 }
