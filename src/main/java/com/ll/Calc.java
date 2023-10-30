3bfeaef6c01c1ef4ca071196a926eda09027b028
harriet221 <potter19@dongguk.edu>
2023-10-30 10:42:37 +0900

    1단계 TDD 10+8==18

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,6 +2,25 @@ package com.ll;
 
 public class Calc {
     public static int run(String exp) {
+        String[] operation = exp.split(" ");
+
+        int a = Integer.parseInt(operation[0]);
+        String op = operation[1];
+        int b = Integer.parseInt(operation[2]);
+
+        if(op.equals("+")) {
+            return a+b;
+        } else if(op.equals("-")) {
+            return a-b;
+        } else if(op.equals("*")) {
+            return a*b;
+        } else if(op.equals("/")) {
+            if(b == 0) {
+                System.out.println("0으로 나누지 말라고");
+                return -999;
+            }
+            return a/b;
+        }
         return 0;
     }
 }
--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -3,10 +3,19 @@ package com.ll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CalcTest {
     @Test
     @DisplayName("Calc의 run 메서드는 int를 return 한다.")
     void test1() {
         int rs = Calc.run("");
+    }
+
+    @Test
+    @DisplayName("10 + 8 == 18")
+    void test2() {
+        int rs = Calc.run("10 + 8");
+        assertThat(rs).isEqualTo(18);
     }
 }
